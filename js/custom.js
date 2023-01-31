adadb50a159f971881183b24b3d9b01010cc004f
agusnat <65430893+agusnat@users.noreply.github.com>
2023-01-30 22:45:42 -0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -361,7 +361,7 @@ jQuery(document).ready(function($)
     }
 
 	function initArrivals() {
-		let sheets = new SheetsJson('18j3DDe7Xorzyy3bzPKBVQ3tc2KCQmmEJNYbzoGp1rtY');
+		let sheets = new SheetsJson('1kJGF5Ml82SEaVGkR-Ws0SNiUhBEPvNcVBwqDJuqaKH4');
 
 		sheets.loadData().then((data) => {
 			data.forEach((row, index) => {
